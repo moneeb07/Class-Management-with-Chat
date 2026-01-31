d6af23cccef5d82b34a863f488aea05f685c315a
Moneeb Ur Rahman <moneeburrahman07@gmail.com>
2026-01-31 21:49:22 +0500

    chnaged clinet url

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 // CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
