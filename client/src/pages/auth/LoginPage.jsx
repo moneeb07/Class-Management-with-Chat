9c7bf0aaa78dfc8c2885d96ed30f146db012b0b0
moneeb07 <moneeburrahman07@gmail.com>
2026-04-19 14:38:15 +0500

    go to form buttom removed

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -8,16 +8,6 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-green-50 to-emerald-100 flex items-center justify-center p-4">
       <LoginForm />
-      <button
-        onClick={() => {
-          navigate("/app");
-          document.cookie =
-            "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=localhost";
-        }}
-        className="mt-4 px-6 py-2 bg-emerald-500 text-white rounded-xl shadow hover:bg-emerald-600 transition"
-      >
-        Go to Form
-      </button>
     </div>
   );
 };
